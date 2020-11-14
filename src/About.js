859831610213e03ef98f8fc35f70edd6ca864b40
Mark Maddison <seawolf98tr@gmail.com>
2020-11-14 23:50:45 +0300

    About Page changed

--- src/About.js
+++ src/About.js
@@ -3,24 +3,24 @@ import {StyleSheet, Text, View, Image,ScrollView} from 'react-native';
 
 const About = (props) => {
   return (
-    <ScrollView style={{flex:1}}>
+    <ScrollView>
 
       <View  style={styles.container}>
-              
         <Image source={require('../src/assets/icon.png')}
         style={{ width: 50, height: 50 }} />
         <Text> Deprem Monitörü v0.2</Text>
-        <Text> Created by  &copy;2020</Text>
+        <Text> Created by  &copy;2020{'\n'}</Text>
         
-        <Text>Veriler Gerçek zamanlı olarak
-        {'\n'}
-        https://deprem.odabas.xyz/api/pure_api.php {'\n'}
-        adresinden alınmaktadır.</Text>
-        
-        <Text>Bu versiyonda 3.5 şiddetinden büyük depremler belirginleştirildi.
-        {'\n'} Depremin bilgisinin üzerine basılınca depremin koordinatları belirtildi.
-        {'\n'}Bir sonraki versiyonda harita verilerinin eklenmesi planlanmaktadır.
-        </Text>
+        <View>
+          <Text>Veriler Gerçek zamanlı olarak
+          {'\n'}https://deprem.odabas.xyz/api/pure_api.php {'\n'}
+          adresinden alınmaktadır.{'\n'}{'\n'}
+          Bu versiyonda :{'\n'}
+          1.) 3.5 şiddetinden büyük depremler belirginleştirildi.{'\n'}
+          {'\n'} 2.) Depremin bilgisinin üzerine basılınca depremin koordinatlarına ulaşılması sağlandı.{'\n'}
+          {'\n'}3.) Bir sonraki versiyonda harita verilerinin eklenmesi planlandı.
+          </Text>
+        </View>  
         <Image source={require('../src/assets/app.jpeg')}
         style={{ width: 80, height: 150 }} />
         <Text style={{color:'#00766c',padding:40,fontWeight:'bold'}} onPress={() => props.navigation.navigate('Main')}>&lt;&lt;&lt; Depremler Sayfasına Dön</Text>
@@ -41,6 +41,7 @@ const styles = StyleSheet.create({
     padding: 5,
     borderRadius: 5,
     alignItems:'center',
-    justifyContent:'flex-start'
+    justifyContent:'flex-start',
+    alignItems:'center'
   },
 });
